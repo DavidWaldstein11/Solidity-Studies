2365abdca30e8c4fa3f54762d37d19b448164777
David Waldstein <78954165+DavidWaldstein11@users.noreply.github.com>
2025-04-20 20:57:25 -0300

    Create basicstructure.sol

---
+++ basics/basicstructure.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;            // Version of Solidity
+
+contract MyContract {              // Smart contract definition
+    uint public myNumber;         // State variable (visible on-chain)
+    string public myText;        // Another state variable
+}
